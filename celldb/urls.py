0a7a65d71a3817d0a01a11cef83abe50e01d0ac8
zhupei <zhupei1355974677@gmail.com>
2023-07-20 20:25:56 +0800

    页面间相互链接

--- celldb/urls.py
+++ celldb/urls.py
@@ -4,13 +4,17 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="celldb-home"),
+    # path("search/", views.search, name="celldb-browse"),
+    path("browse-feature/", views.browseFeature, name="celldb-browse-feature"),
+    path("browse-expression/", views.browseExpression, name="celldb-browse-expression"),
     path("download", views.download, name="celldb-download"),
     path("upload", views.upload, name="celldb-upload"),
     path("overview", views.overview, name="celldb-overview"),
-    path("plot", views.plotScatter, name="celldb-plot"),
+    path("plot/", views.plotScatter, name="celldb-plot"),
     path("plot/local", views.plotLocal, name="celldb-plot-local"),
     path("plot/r", views.plotR, name="celldb-plot-r"),
     path("runcode/", views.runCode, name="celldb-runcode"),
     path("analyse/", views.analyse, name="celldb-analyse"),
     path("test", views.test, name="celldb-test"),
+    path("test2", views.test2, name="celldb-test2"),
 ]
--- singleCell/settings.py
+++ singleCell/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 #For photo File
 import os
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-MEDIA_URL = '/photo/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Photo')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
