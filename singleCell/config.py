47d756b809c39f529016961de9335243edb486de
zhupei <zhupei1355974677@gmail.com>
2023-08-06 06:26:51 -0400

    增加config文件

---
+++ singleCell/config.py
@@ -0,0 +1,7 @@
+DATABASE_ENGINE = 'django.db.backends.mysql'
+DATABASE_NAME = 'scdb'
+DATABASE_USER = 'admin'
+DATABASE_PASSWORD = 'China123'
+DATABASE_HOST = 'aswmysqlsap'
+DATABASE_PORT = '3306'
+DJANGO_SECRET_KEY = "django-insecure-b!vn5)7+smn7ba-@nj0j-!r#4aeh-8d&n!8vju#*+sxeykom#("
